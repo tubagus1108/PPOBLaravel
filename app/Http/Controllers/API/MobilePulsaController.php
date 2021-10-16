e61ad2aaa276ea2b7a9fb97957d7b7054ab28f97
Muhammad Tubagus Sananto <tubagussananto2000@gmail.com>
2021-10-16 09:24:30 +0700

    fix

--- app/Http/Controllers/API/MobilePulsaController.php
+++ app/Http/Controllers/API/MobilePulsaController.php
@@ -5,34 +5,37 @@ namespace App\Http\Controllers\API;
 use App\Http\Controllers\Controller;
 use GuzzleHttp\Client;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
 
 class MobilePulsaController extends Controller
 {
-    private $username = "085760716816";
-    private $api_key = "3815bab21c2ea839";
+    private function Authbalance()
+    {
+        return '{
+            "commands": "balance",
+            "username": "085760716816",
+            "sign": "cde6aec3f75ef1d662c0dcb409da8617"
+        }';
+    }
+    private function AuthPriceList()
+    {
+        return '{
+            "commands" : "pricelist",
+            "username" : "085760716816",
+            "sign"     : "52ae3f052c842ded14b62bb850b39aff",
+            "status"   : "all"
+        }';
+    }
 
     public function checkBelance()
     {
-        $username   = $this->username;
-        $apiKey   = $this->api_key;
-        $signature  = md5($username.$apiKey.'bl');
-        // return $json;
-        $url = "https://testprepaid.mobilepulsa.net/v1/legacy/index";
+        $json = $this->Authbalance();
+        $getBalance = Http::post('https://prepaid.iak.dev/v1/legacy/index', ['body' => $this->Authbalance()]);
+        // dd($getBalance);
+        $json_view = $getBalance->json();
+        return $json_view;
 
-        $ch  = curl_init();
-        curl_setopt($ch, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-        curl_setopt($ch, CURLOPT_URL, $url);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, '{
-            "commands" : "balance",
-            "username" : "085760716816",
-            "sign"     : "cde6aec3f75ef1d662c0dcb409da8617"
-        }');
-        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
-        $data = curl_exec($ch);
-        curl_close($ch);
-        return json_decode($data);
     }
 
 }
