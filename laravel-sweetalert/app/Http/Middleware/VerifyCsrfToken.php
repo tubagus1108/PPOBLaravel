513ff90e6e84543aa9d0335e2996f28182c8cb2c
unknown <tubagussananto2000@gmail.com>
2021-12-17 16:48:54 +0700

    feature deposit

--- laravel-sweetalert/app/Http/Middleware/VerifyCsrfToken.php
+++ laravel-sweetalert/app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'https://tubagusofficial.my.id/callback'
     ];
 }
