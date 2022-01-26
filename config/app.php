75188eefe89f174ea0ba07e5d4a69a0f320109e5
tubagus1108 <tubagussananto2000@gmail.com>
2022-01-26 15:08:27 +0700

    captcha

--- config/app.php
+++ config/app.php
@@ -175,7 +175,6 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         RealRashid\SweetAlert\SweetAlertServiceProvider::class,
-        Mews\Captcha\CaptchaServiceProvider::class,
 
     ],
 
@@ -231,7 +230,6 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
         'Alert' => RealRashid\SweetAlert\Facades\Alert::class,
-        'Captcha' => Mews\Captcha\Facades\Captcha::class,
 
     ],
 
